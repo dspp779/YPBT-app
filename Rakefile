0194332fd3bdfb874aad6a7f8a400d69d4f0b58e
SOA-KunLin <orange6318@hotmail.com>
2016-12-01 23:30:19 +0800

    initialize test

--- Rakefile
+++ Rakefile
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+require 'rake/testtask'
+
+task :default do
+  puts `rake -T`
+end
+
+Rake::TestTask.new(:spec) do |t|
+  t.pattern = 'spec/*_spec.rb'
+  t.warning = false
+end
+
 task :run do
   sh 'rerun "rackup -p 9000"'
 end
---
+++ spec/app_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require_relative 'spec_helper'
+
+describe 'APP basics' do
+  it 'should find configuration information' do
+    app.config.YOUTUBE_API_KEY.length.must_be :>, 0
+    app.config.YPBT_API.length.must_be :>, 0
+  end
+
+  it 'should successfully find the root route' do
+    get '/'
+    last_response.status.must_equal 200
+  end
+end
