acea876bfe832dbcaace34709918c9c8983f4d25
SOA-KunLin <orange6318@hotmail.com>
2016-12-04 20:50:35 +0800

    add channel_id column in video model

--- models/video.rb
+++ models/video.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Video = Struct.new :video_id, :title, :description, :view_count, :like_count,
-                   :dislike_count, :duration, :channel_title,
+                   :dislike_count, :duration, :channel_id, :channel_title,
                    :channel_description, :channel_image_url
