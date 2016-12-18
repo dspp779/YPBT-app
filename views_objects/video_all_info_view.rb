b60195bb90592b4aceecadfdbc92de6b599472a2
JJC <jjc@nlplab.cc>
2016-12-18 17:28:14 +0800

    refactor VideoAllInfoView

--- views_objects/video_all_info_view.rb
+++ views_objects/video_all_info_view.rb
@@ -1,20 +1,28 @@
 # frozen_string_literal: true
 
+# all video info view
 class VideoAllInfoView
   attr_accessor :video_info, :_time_tags
+
   def time_tags
     return _time_tags if _time_tags.empty?
-    like_counts = _time_tags.map(&:like_count)
-    like_min = like_counts.min
-    like_max = like_counts.max
-    like_range = like_max - like_min
-    _time_tags.each do |tag|
-      if like_range.zero?
-        tag.like_color = 160
-      else
-        tag.like_color = 160 - ((tag.like_count - like_min) * 100 / like_range)
-      end
-    end
+    assign_color(_time_tags)
     _time_tags.sort! { |a, b| a.like_color <=> b.like_color }.reverse!
+  end
+
+  def assign_color(time_tags)
+    like_counts = time_tags.map(&:like_count)
+    counts_norm = normalize(like_counts)
+    time_tags.zip(counts_norm).each do |tag, count_norm|
+      tag.like_color = count_norm
+    end
+  end
+
+  def normalize(counts)
+    min = counts.min
+    range = counts.max - min
+    counts.map do |count|
+      160 - ((count - min) * 100 / range)
+    end
   end
 end
